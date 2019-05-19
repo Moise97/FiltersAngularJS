81feea05ac4518030fde7e4e70108e37c4f79df8
Solcan Moise <solcan_moise2014@yahoo.com>
2019-05-19 19:12:32 +0300

    Formatting Dates

--- app/controllers/default-controller.js
+++ app/controllers/default-controller.js
@@ -4,6 +4,11 @@ function($scope, dataService){
     dataService.getData()
         .then(function(result){
             $scope.products = result;
-        })
+        });
+
+    $scope.getExpiryDate = function(days) {
+        let now = new Date();
+        return now.setDate(now.getDate() + days);
+    }
     
 }]);
